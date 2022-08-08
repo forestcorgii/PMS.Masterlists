d4310cdf6fa4deb216e8cd1914a5a88837e65574
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-08-09 00:32:07 +0800

    Added a DbContextFactory.

---
+++ Pms.Employees.Persistence/EmployeeDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+using Pms.Employees.Domain;
+using System;
+
+namespace Pms.Employees.Persistence
+{
+    public class EmployeeDbContextFactory
+    {
+        private readonly string _connectionString = "server=localhost;database=payroll2_efdb;user=root;password=Soft1234;";
+        public EmployeeDbContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public EmployeeDbContext CreateDbContext()
+        {
+            DbContextOptions dbContextOptions = new DbContextOptionsBuilder()
+                .UseMySQL(_connectionString,
+                    options => options.MigrationsHistoryTable("EmployeesMigrationHistoryName")
+                )
+                .Options;
+
+            return new EmployeeDbContext(dbContextOptions);
+        }
+    }
+}
