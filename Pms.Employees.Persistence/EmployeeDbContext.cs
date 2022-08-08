4179f15bcdf9d0155ad26868f74188b4fc7e451b
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-08-09 00:31:44 +0800

    Moved configuration to the DBContextFactory.

--- Pms.Employees.Persistence/EmployeeDbContext.cs
+++ Pms.Employees.Persistence/EmployeeDbContext.cs
@@ -8,14 +8,14 @@ namespace Pms.Employees.Persistence
 {
     public class EmployeeDbContext : DbContext
     {
-         public DbSet<Employee> Employees => Set<Employee>();
-         
-        private readonly string ConnectionString = "server=localhost;database=payroll2_efdb;user=root;password=Soft1234;";
+        public DbSet<Employee> Employees => Set<Employee>();
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-            optionsBuilder.UseMySQL(ConnectionString, options =>
-                options.MigrationsHistoryTable("EmployeesMigrationHistoryName"))
-                .UseLazyLoadingProxies();
+        public EmployeeDbContext(DbContextOptions options) : base(options) { }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        //    optionsBuilder.UseMySQL(ConnectionString, options =>
+        //        options.MigrationsHistoryTable("EmployeesMigrationHistoryName"))
+        //        .UseLazyLoadingProxies();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
