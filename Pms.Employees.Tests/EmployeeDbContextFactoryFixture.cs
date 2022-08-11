7d36adee6d0e736bc57c968ad92e2131e2e0bd95
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-08-11 17:38:58 +0800

    Created a test project.

---
+++ Pms.Employees.Tests/EmployeeDbContextFactoryFixture.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pms.Employees.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pms.Employees.Tests
+{
+    public class EmployeeDbContextFactoryFixture : IDbContextFactory
+    {
+        private const string ConnectionString = "server=localhost;database=payroll3Test_efdb;user=root;password=Soft1234;";
+
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized;
+
+        public EmployeeDbContextFactoryFixture()
+        {
+            CreateFactory();
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateDbContext())
+                    {
+                        context.Database.Migrate();
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+
+                        //context.AddRange( );
+                        //context.SaveChanges();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public EmployeeDbContextFactory Factory;
+        public void CreateFactory()
+            => Factory = new EmployeeDbContextFactory(ConnectionString);
+
+        public EmployeeDbContext CreateDbContext()
+            => Factory.CreateDbContext();
+    }
+}
